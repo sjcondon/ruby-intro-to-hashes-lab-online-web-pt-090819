de16bea25b46f2ca6a8680ecb077e19215f0fe16
sarah <s.j.condon01@gmail.com>
2019-09-25 02:33:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,8 +15,12 @@ end
 
 def monopoly_with_second_tier
 		monopoly = {
-  :railroads => {:pieces => 4, :names => {}
-	}
+      :railroads => {
+        :pieces => 4, 
+        :names => {},
+        
+      }
+	  }
 end
 
 # def monopoly_with_third_tier
