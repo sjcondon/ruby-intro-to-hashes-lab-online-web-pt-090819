a4dbbc7b64b50aed0c8793a48dc8291571929b19
sarah <s.j.condon01@gmail.com>
2019-09-25 02:41:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,10 +18,10 @@ def monopoly_with_second_tier
       :railroads => {
         :pieces => 4, 
         :names => {},
-        :rent_in_dollars => {}
-            :one_piece_owned => 25, 
+        :rent_in_dollars => {
+            one_piece_owned => 25, 
+          }
         }
-        
       }
 	  }
 end
