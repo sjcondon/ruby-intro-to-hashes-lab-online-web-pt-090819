569fdda57f0271550d10b0a15e27304112a10b89
sarah <s.j.condon01@gmail.com>
2019-09-25 03:25:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,7 +33,7 @@ def monopoly_with_third_tier
              :two_pieces_owned => 50,
              :three_pieces_owned => 100,
              :four_pieces_owned => 200,
-          }
+          },
           :names => {
               :reading_railroad => {},
               :pennsylvania_railroad => {},
@@ -44,30 +44,30 @@ def monopoly_with_third_tier
 	}
 end
 
-def monopoly_with_fourth_tier
-	   monopoly = {
-      :railroads => {
-        :pieces => 4, 
-        :rent_in_dollars => {
-             :one_piece_owned => 25,
-             :two_pieces_owned => 50,
-             :three_pieces_owned => 100,
-             :four_pieces_owned => 200,
-          }
-          :names => {
-              :reading_railroad => {
-                "morgage_value" => "$100"
-              },
-              :pennsylvania_railroad => {
-                "morgage_value" => "$200"
-              },
-              :b_and_o_railroad => {
-                "morgage_value" => "$400"
-              },
-              :shortline => {
-                "morgage_value" => "$800"
-              }
-            }
-          }
-		    }
-end
+# def monopoly_with_fourth_tier
+# 	   monopoly = {
+#       :railroads => {
+#         :pieces => 4, 
+#         :rent_in_dollars => {
+#             :one_piece_owned => 25,
+#             :two_pieces_owned => 50,
+#             :three_pieces_owned => 100,
+#             :four_pieces_owned => 200,
+#           }
+#           :names => {
+#               :reading_railroad => {
+#                 "morgage_value" => "$100"
+#               },
+#               :pennsylvania_railroad => {
+#                 "morgage_value" => "$200"
+#               },
+#               :b_and_o_railroad => {
+#                 "morgage_value" => "$400"
+#               },
+#               :shortline => {
+#                 "morgage_value" => "$800"
+#               }
+#             }
+#           }
+# 		    }
+# end
